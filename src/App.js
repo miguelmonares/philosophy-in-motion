aea868433d6078276695c8503b836d614e883c62
mxnares <69873782+mxnares@users.noreply.github.com>
2023-08-04 11:14:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>experience philosophy.</p>
+        <a
+          className="App-link"
+          href="https://github.com/mxnares"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          work in progress
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
